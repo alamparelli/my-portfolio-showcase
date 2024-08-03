7451bbc54d14a3e89f8ec9a6125b3971d4154084
Alessandro LAMPARELLI <alessandro@lamparelli.eu>
2024-08-03 17:12:17 +0200

    last changes for tailwind and removed some uneccessary files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './output.css';
 function App() {
 	return (
 		<div>
-			<header class="text-center">
+			<header class="text-center bg-slate-100">
 				<p class="text-3xl font-italic">Welcome to my Portfolio Showcase</p>
 				<a
 					href="https://github.com/alamparelli"
