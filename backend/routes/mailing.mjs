209413d02d3b5e437622a6080896fc2f4631dd74
Alessandro LAMPARELLI <alessandro@lamparelli.eu>
2024-08-30 11:46:03 +0200

    added mailing functionality with insertion in the db

--- backend/routes/mailing.mjs
+++ backend/routes/mailing.mjs
@@ -1,28 +1,9 @@
-/* eslint-disable sort-imports */
 import express from 'express';
 const router = express.Router();
-import { Contact } from '../views/mailing.mjs';
+import { contactForm } from '../controllers/mailing.mjs';
 
 router.use(express.json());
 
-const getContact = async (req, res) => {
-	try {
-		const answer = await Contact.find();
-		res.status(200).json(answer);
-	} catch (error) {
-		res.status(406).send(error);
-	}
-};
-
-const setContact = async (req, res) => {
-	try {
-		const answer = await Contact.insertMany(req.body);
-		await res.send(answer);
-	} catch (error) {
-		res.status(406).send(error);
-	}
-};
-
-router.route('/mailing').get(getContact).post(setContact);
+router.route('/mailing').post(contactForm);
 
 export default router;
