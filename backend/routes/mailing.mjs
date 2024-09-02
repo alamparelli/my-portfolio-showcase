f9b4d4b39a923d16df302861b040307dc6919e3f
Alessandro LAMPARELLI <alessandro@lamparelli.eu>
2024-09-02 06:15:16 +0200

    moved backend to another folder

--- backend/routes/mailing.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { contactForm } from '../controllers/mailing.mjs';
-
-router.use(express.json());
-
-router.route('/mailing').post(contactForm);
-
-export default router;
